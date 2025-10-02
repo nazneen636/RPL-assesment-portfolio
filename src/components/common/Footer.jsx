4f8e31d535446f15e9293740b187ee318661936c
nazneen636 <nazneennahar636@gmail.com>
2025-10-02 21:18:59 +0600

    footer date

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -3,7 +3,9 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="text-center absolute bottom-0 w-full bg-black text-white py-4">
-      <p className="opacity-80"> Developed by Nazneen Nahar Rima</p>
+      <p className="opacity-80">
+        @ {new Date().getFullYear()} Developed by Nazneen Nahar Rima
+      </p>
     </div>
   );
 };
